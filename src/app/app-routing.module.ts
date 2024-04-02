b6f8bfec512ab8689ba46a6485643001dee53911
weu-souza <weuller1561@gmail.com>
2024-04-02 18:29:25 -0300

    criação do component de navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./Main/home/page/home.component";
 
 
+
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent}
--- src/app/module/app.pages.ts
+++ src/app/module/app.pages.ts
@@ -2,10 +2,10 @@ import {AppComponent} from "../app.component";
 import {HeaderComponent} from "../Main/header/page/header.component";
 import {NavegacaoComponent} from "../Main/nav/navegacao/navegacao.component";
 import {FooterComponent} from "../Main/footer/page/footer.component";
-import {DiscussaoComponent} from "../Main/discussoes/discussao/discussao.component";
-import {HomeComponent} from "../Main/principal/Pages/home.component";
+import {DiscussaoComponent} from "../Main/discussion/page/discussao.component";
+import {HomeComponent} from "../Main/home/page/home.component";
 
-export const PAGES:any[] = [
+export const PAGES: any[] = [
   AppComponent,
   HeaderComponent,
   NavegacaoComponent,
