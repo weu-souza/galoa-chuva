ae11633bed0f8c4b065432869e28364670bdf70a
weu-souza <weuller1561@gmail.com>
2024-04-02 22:51:13 -0300

    criação do component article

--- src/app/Main/article/page/artigo.component.ts
+++ src/app/Main/article/page/artigo.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-artigo',
   templateUrl: './artigo.component.html',
   styleUrls: ['./artigo.component.scss']
 })
-export class ArtigoComponent {
+export class ArtigoComponent implements OnInit {
 
+  private click: boolean;
+
+  verMais: boolean;
+
+  constructor() {
+    this.click = true;
+    this.verMais = false;
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  btnEstrela(): void {
+    this.click = !this.click;
+  }
+
+  btnVerMais(): void {
+    this.verMais = !this.verMais
+  }
+
+  star(): string {
+    if (this.click) {
+      return "pi pi-star"
+    } else {
+      return "pi pi-star-fill"
+    }
+  }
+
+  mostrarEsconder(): string {
+    if (this.verMais) {
+      return ""
+    }
+    return "ver mais"
+  }
 }
